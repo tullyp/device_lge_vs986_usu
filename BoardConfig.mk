312c05d7898128387a544456e8a081c798b1277c
tullyp <54035666+tullyp@users.noreply.github.com>
2021-03-24 22:46:07 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,4 +27,4 @@ TARGET_OTA_ASSERT_DEVICE := g4,p1,vs986_usu
 TARGET_KERNEL_CONFIG := vs986_usu_defconfig
 
 # inherit from the proprietary version
--include vendor/lge/vs986_usu/BoardConfigVendor.mk
+-include vendor/lge/H815/BoardConfigVendor.mk
