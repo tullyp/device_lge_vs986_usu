d25b12cdf591483153b8073c2d4258589d9cd90e
tullyp <54035666+tullyp@users.noreply.github.com>
2021-04-06 15:59:51 -0400

    Update lineage_vs986_usu.mk

--- lineage_vs986_usu.mk
+++ lineage_vs986_usu.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit ROM vendor common stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit ROM vendor telephony stuff
 $(call inherit-product, vendor/havoc/config/telephony.mk)
