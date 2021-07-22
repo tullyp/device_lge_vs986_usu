15b14a510a63f78a6745bb4d3c93e036e99a0730
tullyp <54035666+tullyp@users.noreply.github.com>
2021-07-22 16:32:59 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/lge/vs986/vs986-vendor.mk
+$(call inherit-product-if-exists, vendor/lge/vs986/vs986-vendor.mk)
     
 
 # common g4
